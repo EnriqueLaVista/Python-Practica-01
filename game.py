b189e6b5a5faadedc4b9a50e88455494f06eb054
Enrique La Vista <enriquelavistaolivera@gmail.com>
2023-03-16 17:18:40 -0300

    Arreglar contador de respuestas correctas, y agregar informar respuestas incorrectas.

--- game.py
+++ game.py
@@ -11,13 +11,14 @@ times = 5
 # Esto toma la fecha y hora actual.
 init_time = datetime.now()
 
+correct_amount = 0
+
 print(f"¡Veremos cuanto tardas en responder estas {times} operaciones!")
 for i in range(0, times):
     # Se eligen números y operador al azar
     number_1 = randrange(10)
     number_2 = randrange(10)
     operator = choice(operators)
-    correct_amount = 0
     # Se imprime la cuenta.
     print(f"{i+1}- ¿Cuánto es {number_1} {operator} {number_2}?")
     # Le pedimos al usuario el resultado
@@ -36,7 +37,7 @@ for i in range(0, times):
                 correct_result = number_1 / number_2
     if str(correct_result) == str(result):
         print("Es correcto")
-        correct_amount += 1
+        correct_amount = correct_amount + 1
     else:
         print("Es incorrecto")
 
@@ -46,4 +47,4 @@ end_time = datetime.now()
 # Restando las fechas obtenemos el tiempo transcurrido.
 total_time = end_time - init_time
 # Mostramos ese tiempo en segundos.
-print(f"\n Tardaste {total_time.seconds} segundos y tuviste {correct_amount} respuestas correctas.")
+print(f"\n Tardaste {total_time.seconds} segundos y tuviste {correct_amount} respuestas correctas y {times - correct_amount} respuestas incorrectas.")
