7be4a8e28ef26436e9fe286b3190ad0b7748685b
Enrique La Vista <enriquelavistaolivera@gmail.com>
2023-03-22 17:32:13 -0300

    Mejora de legibilidad y adición de comentarios

--- game.py
+++ game.py
@@ -11,18 +11,24 @@ times = 5
 # Esto toma la fecha y hora actual.
 init_time = datetime.now()
 
+# Cantidad de respuestas correctas.
 correct_amount = 0
 
 print(f"¡Veremos cuanto tardas en responder estas {times} operaciones!")
+
 for i in range(0, times):
     # Se eligen números y operador al azar
     number_1 = randrange(10)
     number_2 = randrange(10)
     operator = choice(operators)
+
     # Se imprime la cuenta.
-    print(f"{i+1}- ¿Cuánto es {number_1} {operator} {number_2}?")
+    print(f"{i + 1}- ¿Cuánto es {number_1} {operator} {number_2}?")
+
     # Le pedimos al usuario el resultado
     result = input("resultado: ")
+
+    # Cálculo de la cuenta.
     match operator:
         case "+":
             correct_result = number_1 + number_2
@@ -35,16 +41,22 @@ for i in range(0, times):
                 correct_result = "Error"
             else:
                 correct_result = number_1 / number_2
+    
+    # Se imprime si el resultado es correcto o no.
     if str(correct_result) == str(result):
         print("Es correcto")
-        correct_amount = correct_amount + 1
+        correct_amount += 1
     else:
         print("Es incorrecto")
 
 # Al terminar toda la cantidad de cuentas por resolver.
 # Se vuelve a tomar la fecha y la hora.
 end_time = datetime.now()
+
 # Restando las fechas obtenemos el tiempo transcurrido.
 total_time = end_time - init_time
+
 # Mostramos ese tiempo en segundos.
-print(f"\n Tardaste {total_time.seconds} segundos y tuviste {correct_amount} respuestas correctas y {times - correct_amount} respuestas incorrectas.")
+print(f"""\nTardaste {total_time.seconds} segundos y 
+ tuviste {correct_amount} respuestas correctas y 
+ {times - correct_amount} respuestas incorrectas.""")
